bbe0469a41f6a30afc24ee6d95306b08c69cc0d2
kanxuravi <kanxuravi9680@gmail.com>
2019-04-15 20:19:29 +0530

    review 1

--- js/pointerstyle.js
+++ js/pointerstyle.js
@@ -1,120 +1,23 @@
-
-(function emojiCursor() {
-  
-  var possibleEmoji = ["0","1"]
-  var width = window.innerWidth;
-  var height = window.innerHeight;
-  var cursor = {x: width/2, y: width/2};
-  var particles = [];
-  
-  function init() {
-    bindEvents();
-    loop();
-  }
-  
-  function bindEvents() {
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('touchmove', onTouchMove);
-    document.addEventListener('touchstart', onTouchMove);
-    
-    window.addEventListener('resize', onWindowResize);
-  }
-  
-  function onWindowResize(e) {
-    width = window.innerWidth;
-    height = window.innerHeight;
-  }
-  
-  function onTouchMove(e) {
-    if( e.touches.length > 0 ) {
-      for( var i = 0; i < e.touches.length; i++ ) {
-        addParticle( e.touches[i].clientX, e.touches[i].clientY, possibleEmoji[Math.floor(Math.random()*possibleEmoji.length)]);
-      }
-    }
-  }
-  
-  function onMouseMove(e) {    
-    cursor.x = e.clientX;
-    cursor.y = e.clientY;
-    
-    addParticle( cursor.x, cursor.y, possibleEmoji[Math.floor(Math.random()*possibleEmoji.length)]);
-  }
-  
-  function addParticle(x, y, character) {
-    var particle = new Particle();
-    particle.init(x, y, character);
-    particles.push(particle);
-  }
-  
-  function updateParticles() {
-  
-    for( var i = 0; i < particles.length; i++ ) {
-      particles[i].update();
-    }
-    
-    for( var i = particles.length -1; i >= 0; i-- ) {
-      if( particles[i].lifeSpan < 0 ) {
-        particles[i].die();
-        particles.splice(i, 1);
-      }
-    }
-    
-  }
-  
-  function loop() {
-    requestAnimationFrame(loop);
-    updateParticles();
-  }
-
-  
-  function Particle() {
-
-    this.lifeSpan = 120; //ms
-    this.initialStyles ={
-      "position": "absolute",
-      "display": "inline-block",
-      "pointerEvents": "none",
-      "z-index": "10000000",
-      "fontSize": "16px",
-      "will-change": "transform"
-    };
-
-    this.init = function(x, y, character) {
-
-      this.velocity = {
-        x:  (Math.random() < 0.5 ? -1 : 1) * (Math.random() / 2),
-        y: 1
-      };
-      
-      this.position = {x: x - 10, y: y - 20};
-
-      this.element = document.createElement('span');
-      this.element.innerHTML = character;
-      applyProperties(this.element, this.initialStyles);
-      this.update();
-      
-      document.body.appendChild(this.element);
-    };
-    
-    this.update = function() {
-      this.position.x += this.velocity.x;
-      this.position.y += this.velocity.y;
-      this.lifeSpan--;
-      
-      this.element.style.transform = "translate3d(" + this.position.x + "px," + this.position.y + "px,0) scale(" + (this.lifeSpan / 120) + ")";
-    }
-    
-    this.die = function() {
-      this.element.parentNode.removeChild(this.element);
-    }
-    
-  }
-
-  function applyProperties( target, properties ) {
-    for( var key in properties ) {
-      target.style[ key ] = properties[ key ];
-    }
-  }
-  
-  init();
-})();
+window.addEventListener('mousemove', function(e) {
+  [1, .9, .8, .5, .1].forEach(function(i) {
+    var j = (1 - i) * 50;
+    var elem = document.createElement('div');
+    var size = Math.ceil(Math.random() * 10 * i) + 'px';
+    elem.style.position = 'fixed';
+    elem.style.top = e.pageY + Math.round(Math.random() * j - j / 2) + 'px';
+    elem.style.left = e.pageX + Math.round(Math.random() * j - j / 2) + 'px';
+    elem.style.width = size;
+    elem.style.height = size;
+    elem.style.background = 'hsla(' +
+      Math.round(Math.random() * 360) + ', ' +
+      '100%, ' +
+      '50%, ' +
+      i + ')';
+    elem.style.borderRadius = size;
+    elem.style.pointerEvents = 'none';
+    document.body.appendChild(elem);
+    window.setTimeout(function() {
+      document.body.removeChild(elem);
+    }, Math.round(Math.random() * i * 500));
+  });
+}, false);
